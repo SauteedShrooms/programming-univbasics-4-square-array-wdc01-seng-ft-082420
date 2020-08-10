661472bc413188af572bbb50924649ff848526e8
Nicholas Baccete <nickbaccete@gmail.com>
2020-08-10 00:33:23 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,3 +1,9 @@
 def square_array(array)
-  # your code here
+  numbers = [1, 2, 3]
+  new_array = []
+  
+  numbers.length.times do 
+    new_array.push(array[index] **[index])
+  end
+  new_array
 end
